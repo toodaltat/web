32549c7debe9336dd5f0796e2a847a336368efe6
E <ethandmilne@gmail.com>
2022-04-23 13:54:11 +1200

    deployed html code

--- 'main'/mysite/main/views.py
+++ 'main'/mysite/main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(request):
-	return HttpResponse("Tech With E!")
+def index(response, name):
+	ls = ToDoList.objects.get(name=name)
+	item = ls.item_set.get(id=1)
+	return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
+
+def home(response):
+	return render(response, "'main'/home.html", {})
