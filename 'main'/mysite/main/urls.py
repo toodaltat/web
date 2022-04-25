488b201b472508d34f4c638c7053672f6e78654b
E <ethandmilne@gmail.com>
2022-04-25 12:20:57 +1200

    Posting and getting forms

--- 'main'/mysite/main/urls.py
+++ 'main'/mysite/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
 	path('<int:id>', views.index, name='index'),
-	path('', views.home, name='home')
+	path('', views.home, name='home'),
+	path('create/', views.home, name='home'),
+	path('<int:id>', views.index, name='index'),
 ]
--- 'main'/mysite/main/views.py
+++ 'main'/mysite/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from .forms import CreateNewList
 
 # Create your views here.
 
@@ -11,3 +12,19 @@ def index(response, name):
 
 def home(response):
 	return render(response, "'main'/home.html", {"name": "E"})
+
+
+def create(response):
+	if response.method == "POST":
+		form = CreateNewList(response.POST)
+
+		if form.is_vaild():
+			n = form.cleaned_data["name"]
+			t = ToDoList(name=n)
+			t.save()
+
+		return HttpResponseRedirect("/%i" %t.id)
+	else:
+		form = CreateNewList()
+
+	return render(response, "'main'/create.html", {"form":form})
